44614a68cc9e44475c87e83579d9104dea358de8
Bonnie Jonkman <bonnie.jonkman@nrel.gov>
2016-07-01 04:48:56 +0000

    * added initOut data for linearization row/column headers * removed some error checks that are unnecessary * changed some variable names to match FAST framework template (and shorten some lines) * modified jacobian routines to reflect some changes in arguments * fixed the sign (i think) on the sine term of propagation direction in jacobian * changed naming of output files because FAST will give the rootname with '.IfW' appended. I didn't change the InflowWind driver, so I modified the CertTest and its results so they don't assume the summary files have an .IfW in their file names.
    git-svn-id: https://windsvn2.nrel.gov/InflowWind/branches/modularization2@207 96535b8f-fecd-4773-a067-ac893715d582

--- CertTest/CertTest.bat
+++ CertTest/CertTest.bat
@@ -14,7 +14,7 @@ echo %pounds%
 
 SET TEST=Test001--steady
 %PROG_EXE% %TEST%.dvr
-%COMPARE% %TEST%.IfW.sum                   %OLD_RESULTS%\%TEST%.IfW.sum               >> CertTest.out
+%COMPARE% %TEST%.sum                       %OLD_RESULTS%\%TEST%.sum                   >> CertTest.out
 %COMPARE% %TEST%.WindGrid.out              %OLD_RESULTS%\%TEST%.WindGrid.out          >> CertTest.out
 %COMPARE% Test001--points.Velocity.dat     %OLD_RESULTS%\Test001--points.Velocity.dat >> CertTest.out
 echo %pounds% >> CertTest.out
@@ -22,7 +22,7 @@ echo %pounds%
 
 SET TEST=Test002--Uniform
 %PROG_EXE% %TEST%.dvr
-%COMPARE% %TEST%.IfW.sum                   %OLD_RESULTS%\%TEST%.IfW.sum               >> CertTest.out
+%COMPARE% %TEST%.sum                       %OLD_RESULTS%\%TEST%.sum                   >> CertTest.out
 %COMPARE% %TEST%.WindGrid.out              %OLD_RESULTS%\%TEST%.WindGrid.out          >> CertTest.out
 
 echo %pounds% >> CertTest.out
@@ -30,14 +30,14 @@ echo %pounds%
 
 SET TEST=Test003--TSFF
 %PROG_EXE% %TEST%.dvr
-%COMPARE% %TEST%.IfW.sum                   %OLD_RESULTS%\%TEST%.IfW.sum               >> CertTest.out
+%COMPARE% %TEST%.sum                       %OLD_RESULTS%\%TEST%.sum                   >> CertTest.out
 %COMPARE% %TEST%.WindGrid.out              %OLD_RESULTS%\%TEST%.WindGrid.out          >> CertTest.out
 echo %pounds% >> CertTest.out
 echo %pounds%
 
 SET TEST=Test004--BladedFF
 %PROG_EXE% %TEST%.dvr
-%COMPARE% %TEST%.IfW.sum                   %OLD_RESULTS%\%TEST%.IfW.sum               >> CertTest.out
+%COMPARE% %TEST%.sum                       %OLD_RESULTS%\%TEST%.sum                   >> CertTest.out
 %COMPARE% %TEST%.WindGrid.out              %OLD_RESULTS%\%TEST%.WindGrid.out          >> CertTest.out
 %COMPARE% Test004--points.Velocity.dat     %OLD_RESULTS%\Test004--points.Velocity.dat >> CertTest.out
 echo %pounds% >> CertTest.out
